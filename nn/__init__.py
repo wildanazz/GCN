0fe5f2b7f8b610038a0e47a277a0ddb811249da3
Muhammad Wildan Aziz <wildanazzwa@gmail.com>
2023-07-13 22:05:43 +0700

    Add functions

--- nn/__init__.py
+++ nn/__init__.py
@@ -1,24 +1,26 @@
 import logging
-
 import azure.functions as func
+
+from .helper import write_to_temp
+from .driver import main as gcn
+from .plot import plot
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    data = req.files['data']
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    if data:
+        write_to_temp(data)
+        try:
+            gcn()
+        except:
+            logging.error('Something went wrong when performing GCN.')
+
+        return func.HttpResponse(f"{data.filename} received")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+            "Please pass valid data in the request body",
+            status_code=400
         )
